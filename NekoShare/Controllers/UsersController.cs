087ace91bab9b65db5a3b427d8be440adbeca508
Henry <viethoang9314@gmail.com>
2024-12-25 21:53:39 +0700

    Add User Controller

---
+++ NekoShare/Controllers/UsersController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using NekoShare.Data;
+using NekoShare.Entities;
+
+namespace NekoShare.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class UsersController(DataContext context) : ControllerBase
+{
+    [HttpGet]
+    public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+    {
+        List<AppUser> users = await context.Users.ToListAsync();
+
+        return users;
+    }
+
+    [HttpGet("{id:int}")]
+    public async Task<ActionResult<AppUser>> GetUserById(int id)
+    {
+        AppUser? user = await context.Users.FindAsync(id);
+
+        return user != null ? user : NotFound();
+    }
+}
