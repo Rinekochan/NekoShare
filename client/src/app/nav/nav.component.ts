c15e9bd1c8814a61c445086cb99561d526dc93b8
Hoang <hoangpv39@fpt.com>
2024-12-30 16:59:52 +0700

    Add Toastr

--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -3,10 +3,14 @@ import {FormsModule} from '@angular/forms';
 import {AccountService} from '../_services/account.service';
 
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
+import {Router, RouterLink, RouterLinkActive} from '@angular/router';
+import {ToastrService} from 'ngx-toastr';
 @Component({
   selector: 'app-nav',
   standalone: true,
   imports: [
+    RouterLink,
+    RouterLinkActive,
     FormsModule,
     BsDropdownModule
   ],
@@ -15,18 +19,21 @@ import { BsDropdownModule } from 'ngx-bootstrap/dropdown'
 })
 export class NavComponent {
   accountService = inject(AccountService);
+  private router = inject(Router);
+  private toastr = inject(ToastrService);
   model: any = {};
 
   login() {
     this.accountService.login(this.model).subscribe({
-      next: response => {
-        console.log(response);
+      next: _ => {
+        this.router.navigateByUrl('/members');
       },
-      error: err => console.log(err)
+      error: err => this.toastr.error(err.error)
     })
   }
 
   logout() {
     this.accountService.logout();
+    this.router.navigateByUrl('/');
   }
 }
--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,6 +1,7 @@
 import {Component, inject, output} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {AccountService} from '../_services/account.service';
+import {ToastrService} from 'ngx-toastr';
 
 @Component({
   selector: 'app-register',
@@ -11,6 +12,7 @@ import {AccountService} from '../_services/account.service';
 })
 export class RegisterComponent {
   private accountService = inject(AccountService);
+  private toastr = inject(ToastrService);
   cancelRegister = output<boolean>();
 
   model: any = {}
@@ -20,7 +22,8 @@ export class RegisterComponent {
       next: response => {
         console.log(response);
         this.cancel();
-      }
+      },
+      error: err => this.toastr.error(err.error)
     })
   }
 
