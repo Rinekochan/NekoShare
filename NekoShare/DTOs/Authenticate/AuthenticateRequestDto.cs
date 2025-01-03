04d7363b98c0cfa672353ae739616ffa13713fe2
Hoang <hoangpv39@fpt.com>
2025-01-03 11:09:22 +0700

    Add server exception handler

--- NekoShare/DTOs/Authenticate/AuthenticateRequestDto.cs
+++ NekoShare/DTOs/Authenticate/AuthenticateRequestDto.cs
@@ -4,9 +4,7 @@ namespace NekoShare.DTOs;
 
 public class AuthenticateRequestDto
 {
-    [Required]
-    public required string Username { get; set; }
-   
-    [Required]
-    public required string Password { get; set; }
+    [Required] public string Username { get; set; } = string.Empty;
+
+    [Required] public string Password { get; set; } = string.Empty;
 }
