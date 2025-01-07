155c3979e534b5cf41a1f4268dcb1e7f828daa6a
Hoang <hoangpv39@fpt.com>
2025-01-07 14:03:26 +0700

    Add members UI, cards and details using ng-gallery

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -2,17 +2,18 @@ import {inject, Injectable, signal} from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import { User } from '../_models/user';
 import {map} from 'rxjs';
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AccountService {
   private http = inject(HttpClient);
-  baseUrl = 'http://localhost:5000/api/';
+  baseUrl = environment.apiUrl;
   currentUser = signal<User | null>(null);
 
   login(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+    return this.http.post<User>(`${this.baseUrl}/account/login`, model).pipe(
       map(user => {
         if(user) {
           localStorage.setItem('user', JSON.stringify(user));
@@ -29,7 +30,7 @@ export class AccountService {
   }
 
   register(model: any) {
-    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+    return this.http.post<User>(`${this.baseUrl}/account/register`, model).pipe(
       map(user => {
         if(user) {
           localStorage.setItem('user', JSON.stringify(user));
--- client/src/app/home/home.component.ts
+++ client/src/app/home/home.component.ts
@@ -1,26 +1,18 @@
-import {Component, inject, OnInit} from '@angular/core';
+import {Component} from '@angular/core';
 import {RegisterComponent} from '../register/register.component';
-import {HttpClient} from '@angular/common/http';
-import {RouterOutlet} from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [
-    RouterOutlet,
     RegisterComponent
   ],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent implements OnInit {
-  http = inject(HttpClient);
+export class HomeComponent {
   registerMode = false;
   users: any;
-
-  ngOnInit(): void {
-    this.getUsers()
-  }
 
   registerToggle() {
     this.registerMode = !this.registerMode;
@@ -28,13 +20,5 @@ export class HomeComponent implements OnInit {
 
   cancelRegisterMode(event: boolean) {
     this.registerMode = event;
-  }
-
-  getUsers() {
-    this.http.get('https://localhost:5001/api/users').subscribe({
-      next: response => this.users = response,
-      error: err => console.log(err),
-      complete: () => console.log("Request has completed")
-    })
   }
 }
